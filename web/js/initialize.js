9cba3957f08ff12bd3d775f7462ddcd5364bc88a
B4dWo1f <B4dWo1f@protonmail.com>
2019-10-30 23:05:12 +0100

    updated web, added colorbars

--- web/js/initialize.js
+++ web/js/initialize.js
@@ -2,11 +2,13 @@ var title_prop = {'blwind':'BL Wind', 'bltopwind':'BL Top Wind', 'sfcwind':'Surf
                   'cape': 'CAPE', 'wstar':'Thermal Updraft Velocity', 'hbl':'Height of BL Top',
                   'bsratio':'Buoyancy/Shear Ratio', 'blcloudpct': '1h Accumulated Rain',
                   'mslpress':'Mean Sea Level Pressure'}
-var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+//var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+var days = ['Domingo','Lunes','Martes','Miércoles','Jueves','Viernes','Sábado'];
 var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
 var SCs = ['SC2', 'SC2+1', 'SC4+2', 'SC4+3']
 
 var today = new Date()
+var UTCshift = today.getTimezoneOffset() / 60;
 var d = today.getDate();
 var dw = days[ today.getDay() ];
 var m = today.getMonth() + 1;
@@ -21,38 +23,70 @@ var domain = 'w2';
 var Oprop = null;
 var Sprop = 'sfcwind';
 var Vprop = 'sfcwind';
-var hour = '12';
+var Ndays = 4;
+var Nhours = 12;
+var hour0 = 8;
+var hour = 12;
 
 var plot_title = document.getElementById("Title");
 
+// Useful aliases
+var TER_layer = document.getElementById('terrain_layer')
+var GND_layer = document.getElementById('gnd_layer')
+var CCA_layer = document.getElementById('ccaa_layer')
+var RIV_layer = document.getElementById('rivers_layer')
+var ROA_layer = document.getElementById('roads_layer')
+var TAK_layer = document.getElementById('takeoffs_layer')
+// data
+var S_layer = document.getElementById('scalar_layer')
+var V_layer = document.getElementById('vector_layer')
+var C_layer = document.getElementById('clouds_layer')
+var P_layer = document.getElementById('press_layer')
+var CB_layer = document.getElementById('cbar_layer')
+
 // Default values for initial load
-document.getElementById('terrain_layer').src = folder+'/'+domain+'/'+sc+'/terrain.png';
-document.getElementById('gnd_layer').src = folder+'/'+domain+'/'+sc+'/terrain1.png';
-document.getElementById('ccaa_layer').src = folder+'/'+domain+'/'+sc+'/ccaa.png';
-document.getElementById('rivers_layer').src = folder+'/'+domain+'/'+sc+'/rivers.png';
-document.getElementById('roads_layer').src = folder+'/'+domain+'/'+sc+'/roads.png';
-document.getElementById('takeoffs_layer').src = folder+'/'+domain+'/'+sc+'/takeoffs.png';
+TER_layer.src = get_folder(folder,domain,sc)+'/terrain.png';
+GND_layer.src = get_folder(folder,domain,sc)+'/terrain1.png';
+CCA_layer.src = get_folder(folder,domain,sc)+'/ccaa.png';
+RIV_layer.src = get_folder(folder,domain,sc)+'/rivers.png';
+ROA_layer.src = get_folder(folder,domain,sc)+'/roads.png';
+TAK_layer.src = get_folder(folder,domain,sc)+'/takeoffs.png';
 // meteo
-document.getElementById('scalar_layer').src =  folder+'/'+domain+'/'+sc+'/'+hour+'00_'+Sprop+'.png';
-document.getElementById('vector_layer').src =  folder+'/'+domain+'/'+sc+'/'+hour+'00_'+Vprop+'_vec.png';
+S_layer.src = get_filename(folder,domain,sc,hour,UTCshift,Sprop,false);
+V_layer.src = get_filename(folder,domain,sc,hour,UTCshift,Vprop,true);
 plot_title.innerHTML = dw+' '+d+' '+title_prop[Sprop]+' '+hour+':00';
 // special layers
-document.getElementById('clouds_layer').src =  folder+'/'+domain+'/'+sc+'/'+hour+'00_blcloudpct.png';
-document.getElementById('press_layer').src =  folder+'/'+domain+'/'+sc+'/'+hour+'00_mslpress.png';
+C_layer.src  = get_filename(folder,domain,sc,hour,UTCshift,'blcloudpct',false);
+P_layer.src  = get_filename(folder,domain,sc,hour,UTCshift,'mslpress',false);
+CB_layer.src = folder+'/'+Sprop+'.png';
 
 // Generate days
 var text = "";
 var i;
-for (i = 0; i <= 3; i++) {
-   text += '<a href="javascript:change_day(' + i.toString() + ');">' 
-   text += days[(( today.getDay() + i )%7)] + "</a> | ";
+for (i = 0; i < Ndays; i++) {
+   // text += '<a class="a_days" href="javascript:change_day(' + i.toString() + ');">' 
+   // text += days[(( today.getDay() + i )%7)] + "</a> | ";
+   text += '<button type="button" class="button_inactive" '
+   text += 'id="button_day_'+i.toString()+'" ';
+   text += 'onclick="javascript:change_day('+i.toString()+');">';
+   text += days[(( today.getDay() + i )%7)];
+   text += '</button>   ';
 }
 document.getElementById("days").innerHTML = text.slice(0, -2);
 
-// Generate hours
+// Generate hours in local time
 var text = "";
 var i;
-for (i = 8; i <= 20; i++) {
-   text += '<a href="javascript:change_hour('+i.toString()+');">' + i.toString() + ":00</a> | ";
+var j;
+for (i = 0; i < Nhours; i++) {
+   j = i + hour0;
+   //text += '<a class="a_hours" href="javascript:change_hour('+j+');">'
+   //text += j + ":00</a> | ";
+   text += '<button type="button" class="button_inactive" '
+   text += 'id="button_hour_'+i.toString()+'" ';
+   text += 'onclick="javascript:change_hour('+i+');">';
+   text += j + ":00";
+   //text += days[(( today.getDay() + i )%7)];
+   text += '</button>   ';
 }
 document.getElementById("hours").innerHTML = text.slice(0, -2);
