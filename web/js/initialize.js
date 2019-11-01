5623dc1af0403de9ba18c9bdc5edefd71c73b4de
Bad Wolf <noel.garcia@openmailbox.org>
2019-11-01 18:06:14 +0100

    external valid dates

--- web/js/initialize.js
+++ web/js/initialize.js
@@ -1,5 +1,3 @@
-var valid_dates = {'SC2': {'w2': new Date('10/31/2019'), 'd2': new Date('10/31/2019')}, 'SC2+1': {'w2': new Date('11/01/2019'), 'd2': new Date('11/01/2019')}, 'SC4+2': {'w2': new Date('11/02/2019'), 'd2': new Date('11/02/2019')}, 'SC4+3': {'w2': new Date('11/03/2019'), 'd2': new Date('11/03/2019')}};
-
 
 var title_prop = {'blwind':'BL Wind', 'bltopwind':'BL Top Wind', 'sfcwind':'Surface Wind',
                   'cape': 'CAPE', 'wstar':'Thermal Updraft Velocity', 'hbl':'Height of BL Top',
