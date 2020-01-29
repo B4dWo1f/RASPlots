98f7a1e8a3cef630a87a3c5a7e08a9e57c288d1c
n03l <B4dWo1f@protonmail.com>
2020-01-29 21:17:32 +0100

    dummy

--- ML/RASPmodels.py
+++ ML/RASPmodels.py
@@ -3,10 +3,27 @@
 
 import tensorflow as tf
 from tensorflow.keras import models
-from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, LSTM
 from tensorflow.keras.layers import Dense, Flatten, Reshape
 import numpy as np
 
+
+def cnn_lstm(size_in,size_out,name='cnn',error='mse'):
+   """
+   error: mse - mean_squared_error
+          mae - mean_absolute_error
+   """
+   model = models.Sequential(name=name)
+   model.add(Conv2D(50, (5,5), activation='relu', input_shape=size_in))
+   model.add(MaxPooling2D(5))
+   model.add(Conv2D(10, (3,3), activation='relu', input_shape=size_in))
+   model.add(MaxPooling2D(2))
+   model.add(Flatten())
+   model.add(Dense(500, activation='relu'))
+   model.add(Dense(np.prod(size_out), activation='relu'))
+   model.add(Reshape(target_shape=size_out))
+   model.compile(optimizer='adam', metrics=['accuracy'], loss=error)
+   return model
 
 def cnn(size_in,size_out,name='cnn',error='mse'):
    """
